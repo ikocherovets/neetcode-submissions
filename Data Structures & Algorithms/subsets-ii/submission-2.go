f2b22bedf7e6745de458329861b498bb15b32f11
Illia Kocherovets <164780144+ikocherovets@users.noreply.github.com>
2026-07-08 11:29:37 +0200

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.go
@@ -0,0 +1,25 @@
+func subsetsWithDup(nums []int) [][]int {
+    res := [][]int{}
+	sort.Ints(nums)
+
+    var backtrack func(int, []int)
+    backtrack = func(i int, subset []int) {
+        if i == len(nums) {
+            res = append(res, append([]int{}, subset...))
+            return
+        }
+
+        subset = append(subset, nums[i])
+        backtrack(i+1, subset)
+        subset = subset[:len(subset)-1]
+
+        for i+1 < len(nums) && nums[i] == nums[i+1] {
+            i++
+        }
+
+        backtrack(i+1, subset)
+    }
+
+    backtrack(0, []int{})
+    return res
+}
