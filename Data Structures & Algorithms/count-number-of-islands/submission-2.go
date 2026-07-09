5ca347a51538361b0d2774280f98b2e6bd67fdbd
Illia Kocherovets <164780144+ikocherovets@users.noreply.github.com>
2026-07-09 18:33:30 +0200

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.go
@@ -0,0 +1,49 @@
+type pair struct {
+    x int
+    y int
+}
+func numIslands(grid [][]byte) int {
+    if len(grid) == 0 {
+        return 0
+    }
+    rows, cols := len(grid), len(grid[0])
+    visited := make(map[pair]bool)
+    islands := 0
+
+    var bfs func(r, c int)
+    bfs = func(r,c int) {
+        queue := []pair{}
+        queue = append(queue, pair{r,c})
+
+        visited[pair{r,c}] = true
+        
+        for len(queue) > 0 {
+            p := queue[0]
+            queue = queue[1:]
+            dirs := [][]int{{1,0}, {-1,0}, {0,1},{0,-1}}
+            for _, dir := range dirs {
+                r = p.x + dir[0]
+                c = p.y + dir[1]
+                if r >= 0 && 
+                    c >= 0 && 
+                    r < rows && 
+                    c < cols &&
+                    grid[r][c] == byte('1') &&
+                    !visited[pair{r,c}] {
+                        visited[pair{r,c}] = true
+                        queue = append(queue, pair{r,c})
+                    }
+            }
+        }
+    }
+
+    for r := range rows {
+        for c := range cols {
+            if grid[r][c] == byte('1') && !visited[pair{r,c}]{
+                bfs(r,c)
+                islands++
+            }
+        }
+    }
+    return islands
+}
