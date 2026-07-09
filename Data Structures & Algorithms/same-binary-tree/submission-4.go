21430bec550d79c937bec13a325858c989323969
Illia Kocherovets <164780144+ikocherovets@users.noreply.github.com>
2026-07-09 10:39:17 +0200

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.go
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+1. контракт (що робить dfs функція для цього root?)
+   - перевіряє чи p.Val == q.Val
+2. що вона повертає?
+   - bool (true/false)
+
+3. base case для поточного вузла
+   - якщо значення обох нод nill одночасно  {
+    retrun false
+   }
+
+4. що "повертають" діти? (leap of faith)
+   - bool.  dfs(root.Left) та dfs(root.Right)
+     (це і є leap of faith)
+
+5. combine
+   - мені треба глянути тепер чи p.Val == q.Val && isLeftValid && isRightValid.
+*/  
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    var dfs func(p *TreeNode, q *TreeNode) bool 
+    dfs = func(p *TreeNode, q *TreeNode) bool {
+        // Насправді base case — це не обов'язково один if. 
+        // Це всі випадки, коли рекурсія не йде глибше і функція може одразу повернути відповідь.
+        if p == nil && q == nil {
+            return true
+        }
+        if p != nil && q == nil || p == nil && q != nil {
+            return false
+        }
+
+        isValid := p.Val == q.Val
+        isLeftSame := dfs(p.Left, q.Left)
+        isRightSame := dfs(p.Right, q.Right)
+        return isValid && isLeftSame && isRightSame
+
+    }
+
+    return dfs(p, q)
+}
