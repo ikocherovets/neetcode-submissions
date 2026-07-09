42cf46379f7b99d69070fc22c5b08bc1e57a72a9
Illia Kocherovets <164780144+ikocherovets@users.noreply.github.com>
2026-07-09 09:33:14 +0200

    Add: invert-a-binary-tree - submission-6

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-6.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func invertTree(root *TreeNode) *TreeNode {
+	/* 
+	1. контракт (що робить ця функція для цього root?) - функція dfs міняє місцями дві ноди
+	2. що вона повертає - нічого
+	3. base case - root == nil
+	4. що повертають діти - нічого (тут починається leap of faith - recursion)
+	5. combine - як зліпити відповідь маючи left & right? - вже все зроблено
+	*/
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		root.Right, root.Left = root.Left, root.Right
+		dfs(root.Right)
+		dfs(root.Left)
+	}
+    
+	dfs(root)
+	return root
+}
