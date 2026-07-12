d121de4a46077569d530af75c80e3ef36c4e8c63
Illia Kocherovets <164780144+ikocherovets@users.noreply.github.com>
2026-07-12 18:05:28 +0200

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.go
@@ -0,0 +1,24 @@
+func search(nums []int, target int) int {
+    l, r := 0, len(nums)-1
+    for l <= r {
+        mid := (l + r) / 2
+        if target == nums[mid] {
+            return mid
+        }
+
+        if nums[l] <= nums[mid] { // ліва половина відсортована
+            if target > nums[mid] || target < nums[l] {
+                l = mid + 1 // target поза [nums[l], nums[mid]] → праворуч
+            } else {
+                r = mid - 1 // target усередині лівої → ліворуч
+            }
+        } else { // права половина відсортована
+            if target < nums[mid] || target > nums[r] {
+                r = mid - 1 // target поза [nums[mid], nums[r]] → ліворуч
+            } else {
+                l = mid + 1 // target усередині правої → праворуч
+            }
+        }
+    }
+    return -1
+}
