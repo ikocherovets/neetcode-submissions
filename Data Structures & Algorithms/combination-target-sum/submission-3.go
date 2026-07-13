aca1da31168be0a98ddd81661e3562197af6a20b
Illia Kocherovets <164780144+ikocherovets@users.noreply.github.com>
2026-07-13 12:55:13 +0200

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.go
@@ -0,0 +1,32 @@
+func combinationSum(nums []int, target int) [][]int {
+    result := [][]int{}
+	// contract -> повертає всі можливі комбінації для nums[i:] щоб сума == target. Тому треба 2 змінна - currentSlice and total
+	// можна повторювати числа!!!
+	var dfs func(i int, cur []int, total int) 
+	dfs = func(i int, cur []int, total int) {
+		if total == target {
+			temp := make([]int, len(cur))
+			copy(temp, cur)
+			result = append(result, temp)
+			return
+		}
+
+		if total > target || i >= len(nums) {
+			return
+		}
+
+		// не додаю індекс оновлюю total бо current стає тим що в nums[i]
+		cur = append(cur, nums[i])
+		dfs(i, cur, total + nums[i])
+
+		// backtracking - тепер якщо рухаюся далі
+		// це все випливає з контракту чому така поведінка
+		cur = cur[:len(cur) - 1]
+		dfs(i + 1, cur, total)
+	}
+
+
+	dfs(0, []int{}, 0)
+
+	return result
+}
