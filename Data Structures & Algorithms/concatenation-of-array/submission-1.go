7ab098cd659d13904136bf232f10dd032466825f
Illia Kocherovets <164780144+ikocherovets@users.noreply.github.com>
2026-07-16 22:08:07 +0200

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.go
@@ -0,0 +1,11 @@
+func getConcatenation(nums []int) []int {
+    n := len(nums)
+	result := make([]int, 2*len(nums))
+
+	for i, num := range nums {
+		result[i] = num
+		result[i + n] = num
+	}
+
+	return result
+}	
